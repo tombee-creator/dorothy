18fab62a9cd0f9a8dfce15394ef9cb38e58d0adb
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-08-18 16:47:41 +0900

    配列用のテストを実装

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ function try() {
     ./cpu $1
 }
 
-scripts=('test001' 'test002')
-ans=(3 55)
+scripts=('test001' 'test002' 'test003')
+ans=(3 55 3)
 
 for i in $(seq 0 ${#scripts[@]}); do
     ./compiler script/${scripts[$i]}.txt bin/${scripts[$i]}
