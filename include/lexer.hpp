009e440e4877187ead4d8090d191d082cc306f34
tombee-creator <tomomail1997@gmail.c.om>
2023-04-21 20:46:44 +0900

    add header for lexer

---
+++ include/lexer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+namespace Dorothy {
+    class Lexer {
+    public:
+        class Token;
+
+        std::vector<Token> lex(char *);
+    private:
+        std::vector<Token> tokens;
+        int currentIndex = 0;
+
+        bool tokenizeInteger(char *);
+
+        bool isDigit(char);
+        void copyString(char *from, char *to, int length);
+    };
+}
+
+#include "token.hpp"
