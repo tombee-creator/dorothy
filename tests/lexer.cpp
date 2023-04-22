32eab682c3903ad7ece78d6493d06921751c1b5a
tombee-creator <tomomail1997@gmail.c.om>
2023-04-22 15:02:54 +0900

    add test

--- tests/lexer.cpp
+++ tests/lexer.cpp
@@ -7,3 +7,11 @@ TEST(Lexer, LexerTest1){
   auto tokens = lexer.lex("11");
   EXPECT_EQ(tokens.size(), 1);
 }
+
+TEST(Lexer, LexerLexTest01){
+  Lexer lexer;
+  auto tokens = lexer.lex("11");
+  EXPECT_EQ(tokens.size(), 1);
+  EXPECT_EQ(tokens[0].getType(), Lexer::Token::Type::TK_INT);
+  EXPECT_EQ(tokens[0].getInteger(), 11);
+}
