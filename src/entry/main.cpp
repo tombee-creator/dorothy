127d48127f4a7b0f1e49c29f62887ccdaa72dacc
tombee-creator <tomomail1997@gmail.c.om>
2023-04-22 14:40:39 +0900

    add target

--- src/entry/main.cpp
+++ src/entry/main.cpp
@@ -9,6 +9,7 @@ main(int argc, char** argv) {
     int tokenLength;
     Lexer lexer;
     auto tokens = lexer.lex(argv[1]);
+    cout << "target triple = \"arm64-apple-macosx13.0.0\"" << endl;
     cout << "define i32 @main()" << endl;
     cout << "{" << endl;
     cout << "ret i32 " << tokens[0].getInteger() << endl;
