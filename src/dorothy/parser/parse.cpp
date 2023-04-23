a5805bb7c7f75d8ba21ced569ce58dcfe3381cc4
tombee-creator <tomomail1997@gmail.c.om>
2023-04-23 10:33:46 +0900

    change directory

--- src/dorothy/parser/parse.cpp
+++ src/dorothy/parser/parse.cpp
@@ -1,15 +1,11 @@
-#include "../../include/parser.hpp"
+#include "../../../include/parser.hpp"
 using namespace Dorothy;
 
-template<>
-IntegerExpression *
-Parser::parse<IntegerExpression>(vector<Lexer::Token> tokens) {
+CREATE_PARSE_FUNCTION(IntegerExpression) {
     return new IntegerExpression(tokens[_currentIndex].getInteger());
 }
 
-template<>
-Program *
-Parser::parse<Program>(vector<Lexer::Token> tokens) {
+CREATE_PARSE_FUNCTION(Program) {
     if(canParse<Program>(tokens)) {
         Program *program = new Program(
             parse<START_AST_NODE>(tokens));
