9f5c68f70694e491325a069bb290b5d13c84cc36
tombee-creator <tomomail1997@gmail.c.om>
2023-05-04 23:16:41 +0900

    add parse function

--- src/dorothy/parser/parse.cpp
+++ src/dorothy/parser/parse.cpp
@@ -2,12 +2,39 @@
 using namespace Dorothy;
 
 CREATE_PARSE_FUNCTION(IntegerExpression) {
-    return new IntegerExpression(tokens[_currentIndex].getInteger());
+    auto expr = new IntegerExpression(tokens[_currentIndex].getInteger());
+    _currentIndex++;
+    return expr;
+}
+
+CREATE_PARSE_FUNCTION(Expression) {
+    if(canParse<IntegerExpression>(tokens)) return parse<IntegerExpression>(tokens);
+    else {
+        cerr << "* UNREACHABLE * @" << __FILE__ << ": " << __LINE__ << endl;
+        exit(-1);
+    }
+}
+
+CREATE_PARSE_FUNCTION(ReturnStatement) {
+    if(tokens[_currentIndex].getType() != Token::TK_RET) {
+        cerr << "* UNREACHABLE * @" << __FILE__ << ": " << __LINE__ << endl;
+        exit(-1);
+    }
+    _currentIndex++;
+    return new ReturnStatement(parse<Expression>(tokens));
+}
+
+CREATE_PARSE_FUNCTION(Statement) {
+    if(canParse<ReturnStatement>(tokens)) return parse<ReturnStatement>(tokens);
+    else {
+        cerr << "* UNREACHABLE * @" << __FILE__ << ": " << __LINE__ << endl;
+        exit(-1);
+    }
 }
 
 CREATE_PARSE_FUNCTION(Program) {
     if(canParse<Program>(tokens)) {
-        Program *program = new Program(
+        return new Program(
             parse<START_AST_NODE>(tokens));
     }
 }
